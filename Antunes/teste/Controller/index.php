4f8231a589e5802f150c72ff71708b63e485ba72
Raphael Antunes <faelpinheiroantunes@gmail.com>
2023-12-25 23:22:31 -0300

    Deply logica e funções onde define o produto na homepage, e configura a atualização do estoque

---
+++ Antunes/teste/Controller/index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Antunes\test\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
---
+++ Antunes/teste/view/frontend/web/js/custom-component.js
@@ -0,0 +1,22 @@
+define(['jquery', 'uiComponent', 'ko'], function ($, Component, ko) {
+    'use strict';
+    
+    return Component.extend({
+
+        updateStock: function () {
+            var stockEndpoint = '/rest/V1/test/publicendpoint'; // Substitua pelo endpoint correto
+            $.ajax({
+                url: stockEndpoint,
+                type: 'GET',
+                contentType: 'application/json',
+                success: function (response) {
+                    console.log('Estoque atualizado com sucesso:', response);
+                    $('#labelstock').text(response[0]);
+                },
+                error: function (error) {
+                    console.error('Erro ao atualizar o estoque:', error);
+                }
+            });
+        }
+    });
+});
